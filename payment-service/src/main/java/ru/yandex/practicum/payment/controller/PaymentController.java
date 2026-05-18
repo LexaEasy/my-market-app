a318b67c0f3fde05b4dbfc0c960b460753235859
Alexey <al-dyv-16@mail.ru>
2026-05-18 18:03:41 +0300

    Реализовать логику и тесты сервиса платежей

---
+++ payment-service/src/main/java/ru/yandex/practicum/payment/controller/PaymentController.java
@@ -0,0 +1,43 @@
+package ru.yandex.practicum.payment.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import ru.yandex.practicum.payment.generated.api.PaymentsApi;
+import ru.yandex.practicum.payment.generated.model.BalanceResponse;
+import ru.yandex.practicum.payment.generated.model.PaymentRequest;
+import ru.yandex.practicum.payment.generated.model.PaymentResponse;
+import ru.yandex.practicum.payment.service.PaymentService;
+
+@RestController
+public class PaymentController implements PaymentsApi {
+
+	private final PaymentService paymentService;
+
+	public PaymentController(PaymentService paymentService) {
+		this.paymentService = paymentService;
+	}
+
+	@Override
+	public Mono<ResponseEntity<BalanceResponse>> getBalance(ServerWebExchange exchange) {
+		return paymentService.getBalance()
+				.map(balance -> ResponseEntity.ok(new BalanceResponse(balance)));
+	}
+
+	@Override
+	public Mono<ResponseEntity<PaymentResponse>> pay(
+			Mono<PaymentRequest> paymentRequest,
+			ServerWebExchange exchange
+	) {
+		return paymentRequest
+				.flatMap(request -> paymentService.pay(request.getAmount()))
+				.map(result -> {
+					PaymentResponse response = new PaymentResponse(result.success(), result.balance())
+							.message(result.message());
+					HttpStatus status = result.success() ? HttpStatus.OK : HttpStatus.CONFLICT;
+					return ResponseEntity.status(status).body(response);
+				});
+	}
+}

5bfa526649bc025303df8051143b97244f4469cf
Kudinov Alexey <alexey.kudinov@ax-team.corp>
2026-06-01 16:38:55 +0300

    добавление владельца корзин и заказов

--- market-app/src/main/java/ru/yandex/practicum/mymarket/model/Order.java
+++ market-app/src/main/java/ru/yandex/practicum/mymarket/model/Order.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.mymarket.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.ArrayList;
@@ -13,18 +14,27 @@ public class Order {
     @Id
     private Long id;
 
+    @Column("user_id")
+    private Long userId;
+
     @Transient
     private List<OrderItem> items = new ArrayList<>();
 
     protected Order() {
     }
 
-    public static Order create() {
-        return new Order();
+    public static Order create(Long userId) {
+        Order order = new Order();
+        order.userId = userId;
+        return order;
     }
 
     public Long getId() {
         return id;
+    }
+
+    public Long getUserId() {
+        return userId;
     }
 
     public List<OrderItem> getItems() {
--- market-app/src/main/java/ru/yandex/practicum/mymarket/repository/OrderRepository.java
+++ market-app/src/main/java/ru/yandex/practicum/mymarket/repository/OrderRepository.java
@@ -2,9 +2,12 @@ package ru.yandex.practicum.mymarket.repository;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.yandex.practicum.mymarket.model.Order;
 
 public interface OrderRepository extends ReactiveCrudRepository<Order, Long> {
 
-    Flux<Order> findAllByOrderByIdAsc();
+    Flux<Order> findAllByUserIdOrderByIdAsc(long userId);
+
+    Mono<Order> findByIdAndUserId(long id, long userId);
 }

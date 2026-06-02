199d1c527767e66323ee13c58567eeaeba94c6fe
Kudinov Alexey <alexey.kudinov@ax-team.corp>
2026-06-02 11:23:36 +0300

    настройка logout через keycloak

--- market-app/src/main/java/ru/yandex/practicum/mymarket/config/SecurityConfig.java
+++ market-app/src/main/java/ru/yandex/practicum/mymarket/config/SecurityConfig.java
@@ -1,25 +1,38 @@
 package ru.yandex.practicum.mymarket.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(
-            ServerHttpSecurity http,
-            ReactiveClientRegistrationRepository clientRegistrationRepository
+    private final ReactiveClientRegistrationRepository clientRegistrationRepository;
+    private final String keycloakLogoutUri;
+
+    public SecurityConfig(
+            ReactiveClientRegistrationRepository clientRegistrationRepository,
+            @Value("${app.security.keycloak-logout-uri}") String keycloakLogoutUri
     ) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+        this.keycloakLogoutUri = keycloakLogoutUri;
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
@@ -30,20 +43,34 @@ public class SecurityConfig {
                         .anyExchange().permitAll()
                 )
                 .oauth2Login(Customizer.withDefaults())
+                .oauth2Client(Customizer.withDefaults())
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository))
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
                 )
                 .build();
     }
 
-    @Bean
-    public ServerLogoutSuccessHandler oidcLogoutSuccessHandler(
-            ReactiveClientRegistrationRepository clientRegistrationRepository
-    ) {
+    private ServerLogoutSuccessHandler oidcLogoutSuccessHandler() {
         OidcClientInitiatedServerLogoutSuccessHandler successHandler =
-                new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository);
+                new OidcClientInitiatedServerLogoutSuccessHandler(oidcLogoutClientRegistrationRepository());
         successHandler.setPostLogoutRedirectUri("{baseUrl}/");
         return successHandler;
+    }
+
+    private ReactiveClientRegistrationRepository oidcLogoutClientRegistrationRepository() {
+        return registrationId -> clientRegistrationRepository.findByRegistrationId(registrationId)
+                .map(this::withEndSessionEndpoint);
+    }
+
+    private ClientRegistration withEndSessionEndpoint(ClientRegistration clientRegistration) {
+        Map<String, Object> metadata = new HashMap<>(
+                clientRegistration.getProviderDetails().getConfigurationMetadata()
+        );
+        metadata.put("end_session_endpoint", keycloakLogoutUri);
+
+        return ClientRegistration.withClientRegistration(clientRegistration)
+                .providerConfigurationMetadata(metadata)
+                .build();
     }
 }
